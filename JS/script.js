9029f3ab800142e3cb1ca0a8dab7466d2b11a4a9
Alan <tiseiraalan2201@gmail.com>
2024-05-23 22:52:11 -0300

    Revert "Se añadieron las carpetas CSS, JS e IMG y se hizo un HTML BASICO y un JS"
    This reverts commit 3a2deb0306f5efc08056223d794f3069f19f1061.

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-const abrirMenu = document.getElementById('menu-telefonos');
-const navLinks = document.querySelector('.nav-links');
-
-abrirMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
-//ESTO SIRVE PARA QUE SE OCULTEN LOS LINKS DE LA BARRA DE NAVEGACION.
